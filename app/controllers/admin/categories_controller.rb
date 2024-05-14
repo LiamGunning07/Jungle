bf277d5eb21226f48156fde0185fcd74e4120813
LiamGunning07 <liamgunning07@gmail.com>
2024-05-14 11:37:28 -0600

    Added user authentication

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -20,13 +20,14 @@ class Admin::CategoriesController < ApplicationController
   end
   private
 
-def category_params
+  def category_params
   params.require(:category).permit(:name)
-end
-def authenticate
+  end
+
+  def authenticate
   authenticate_or_request_with_http_basic do |username, password|
     username == ENV['ADMIN_USER'] && password == ENV['ADMIN_PASS']
+    end
   end
-end
 
 end
